28d1c26cd455e0068ac7e0ee99b05562c2cbba58
ProfiiDev <92174452+Profiidev@users.noreply.github.com>
2024-09-05 13:10:28 +0200

    -

--- lua/plugins/mason.lua
+++ lua/plugins/mason.lua
@@ -156,9 +156,6 @@ return {
   {
     'rust-lang/rust.vim',
     ft = "rust",
-    init = function ()
-      vim.g.rustfmt_autosave = 1
-    end
   },
 
   {
--- lua/plugins/session.lua
+++ lua/plugins/session.lua
@@ -1,3 +1,9 @@
 return {
   'rmagatti/auto-session',
+  lazy = false,
+  config = function ()
+    require('auto-session').setup({
+      enabled = true,
+    })
+  end
 }
