206d0cbf09e30d5c1c5ce3bf4386bbff0144acc1
ProfiiDev <92174452+Profiidev@users.noreply.github.com>
2024-09-09 17:34:17 +0200

    .

--- lua/plugins/auto_pairs.lua
+++ lua/plugins/auto_pairs.lua
@@ -1,3 +1,9 @@
 return {
-  'jiangmiao/auto-pairs'
+  'jiangmiao/auto-pairs',
+  event = 'InsertEnter',
+  -- Optional dependency
+  dependencies = { 'hrsh7th/nvim-cmp' },
+  config = function()
+    require('nvim-autopairs').setup {}
+  end,
 }
